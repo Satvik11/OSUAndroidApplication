65ff0e49fe797f56b7b7cdacae1398325f176bd1
Satvik11 <kethireddy.satvik@gmail.com>
2017-04-15 14:22:56 -0400

    Recent Version

--- HackathonAndroidApplication/src/com/example/hackathonandroidapplication/WeatherData.java
+++ HackathonAndroidApplication/src/com/example/hackathonandroidapplication/WeatherData.java
@@ -1,7 +1,13 @@
 package com.example.hackathonandroidapplication;
 import android.net.Uri;
+
 import android.os.Bundle;
-import android.
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
 public class WeatherData{
 	
 	static String weatherKey = "79eff3c2692ee9e076066c4f25ddc98d";
@@ -10,19 +16,25 @@ public class WeatherData{
 		// Get Weather Data
 		double temp = 0;
 		String url = "http://samples.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&appid=" + weatherKey;
-		return temp;
+		return 0.0;
 		
-	}
-	StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+	} 
+	
+	StringRequest stringRequest = new StringRequest(Request.Method.GET, "http://samples.openweathermap.org/data/2.5/weather?id=2172797&appid=b1b15e88fa797225412429c1c50c122a1",
             new Response.Listener<String>() {
     @Override
     public void onResponse(String response) {
         // Display the first 500 characters of the response string.
-        //String recivedjsn = mTextView.setText( response.substring(0,500));
-    	System.out.println(response);
+        //mTextView.setText("Response is: "+ response.substring(0,500));
+    	Log.d("TAG",response);
     }
+}, new Response.ErrorListener() {
+    @Override
+    public void onErrorResponse(VolleyError error) {
+       // mTextView.setText("That didn't work!");
+    }
+});
 }
-
 
 
 
